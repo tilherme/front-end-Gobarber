0c1b69dfeecab3ede11b7b014436fc20b87966f0
tilherme <gui.mauricio@hotmail.com.br>
2021-01-03 20:14:30 -0300

    pagina cadastro pronta

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
-import Singin from './pages/Signin';
+//import Singin from './pages/Signin';
+import SignUp from './pages/SignUp';
 const App: React.FC = () => (
   <>
-    <Singin/>
+    <SignUp/>
     <GlobalStyle/>
   
   </>
